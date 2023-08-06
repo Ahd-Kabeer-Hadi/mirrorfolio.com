36084bbab9443f794a0a96c5d972bf0a5cb86248
Ahd.Kabeer Hadi <ahd.kabeerpi@gmail.com>
2023-08-06 13:42:04 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+  // Your other Next.js configurations go here, if any.
+  // For example, you can set up custom routes, environment variables, etc.
+
+  // Add the following configuration to enable static HTML export.
+  // It disables server-side rendering (SSR) and client-side navigation for static exports.
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' },
+      // Add other routes as needed.
+      // Example: '/about': { page: '/about' },
+      // '/contact': { page: '/contact' },
+    };
+  },
+
+  // If you have any other Next.js configurations, include them here as well.
+};
